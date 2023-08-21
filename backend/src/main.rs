e1a703c4ff8aeb726122b30dab48cac827be4de7
Awad <34748292+Awaddd@users.noreply.github.com>
2023-08-21 03:13:49 +0100

    Add axum and basic routes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,15 +7,32 @@ mod models {
   pub mod book;
 }
 
-use std::error::Error;
+use std::net::SocketAddr;
+use axum::{Router, response::{Html, IntoResponse}, routing::get};
+
 use crate::services::books::BookService;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() {
     println!("Hello, world!");
 
-    let books = BookService::get_books().await;
-    print!("books {:?}", books);
+    let app = Router::new()
+      .route("/", get(base_route))
+      .route("/books", get(books));
 
-    Ok(())
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    println!("Listening on {addr}\n");
+
+    axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+}
+
+async fn base_route() -> impl IntoResponse {
+  Html(String::from("Hello World! Connected"))
+}
+
+async fn books()  -> impl IntoResponse {
+  // currently does not return anything
+  let books = BookService::get_books().await;
+  print!("books {:?}", books);
 }
